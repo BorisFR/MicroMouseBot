31281cdb68d4ab621cec436001c6adf783b60b82
BorisFR <sfardoux@hotmail.com>
2026-02-25 12:27:39 +0100

    Enhance TheCompass class with detailed sensor configuration logging and initialize IMU settings in new documentation file

--- src/Sensors/TheCompass.h
+++ src/Sensors/TheCompass.h
@@ -47,16 +47,232 @@ public:
         changeDetected = true;
         if (theCallbackValueChange)
             theCallbackValueChange(currentHeading);
+
+        // lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+        myTrace.print("Accelerometer range set to: ");
+        switch (lsm6ds.getAccelRange())
+        {
+        case LSM6DS_ACCEL_RANGE_2_G:
+            myTrace.println("+-2G");
+            break;
+        case LSM6DS_ACCEL_RANGE_4_G:
+            myTrace.println("+-4G");
+            break;
+        case LSM6DS_ACCEL_RANGE_8_G:
+            myTrace.println("+-8G");
+            break;
+        case LSM6DS_ACCEL_RANGE_16_G:
+            myTrace.println("+-16G");
+            break;
+        }
+
+        // lsm6ds.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+        myTrace.print("Accelerometer data rate set to: ");
+        switch (lsm6ds.getAccelDataRate())
+        {
+        case LSM6DS_RATE_SHUTDOWN:
+            myTrace.println("0 Hz");
+            break;
+        case LSM6DS_RATE_12_5_HZ:
+            myTrace.println("12.5 Hz");
+            break;
+        case LSM6DS_RATE_26_HZ:
+            myTrace.println("26 Hz");
+            break;
+        case LSM6DS_RATE_52_HZ:
+            myTrace.println("52 Hz");
+            break;
+        case LSM6DS_RATE_104_HZ:
+            myTrace.println("104 Hz");
+            break;
+        case LSM6DS_RATE_208_HZ:
+            myTrace.println("208 Hz");
+            break;
+        case LSM6DS_RATE_416_HZ:
+            myTrace.println("416 Hz");
+            break;
+        case LSM6DS_RATE_833_HZ:
+            myTrace.println("833 Hz");
+            break;
+        case LSM6DS_RATE_1_66K_HZ:
+            myTrace.println("1.66 KHz");
+            break;
+        case LSM6DS_RATE_3_33K_HZ:
+            myTrace.println("3.33 KHz");
+            break;
+        case LSM6DS_RATE_6_66K_HZ:
+            myTrace.println("6.66 KHz");
+            break;
+        }
+
+        // lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS );
+        myTrace.print("Gyro range set to: ");
+        switch (lsm6ds.getGyroRange())
+        {
+        case LSM6DS_GYRO_RANGE_125_DPS:
+            myTrace.println("125 degrees/s");
+            break;
+        case LSM6DS_GYRO_RANGE_250_DPS:
+            myTrace.println("250 degrees/s");
+            break;
+        case LSM6DS_GYRO_RANGE_500_DPS:
+            myTrace.println("500 degrees/s");
+            break;
+        case LSM6DS_GYRO_RANGE_1000_DPS:
+            myTrace.println("1000 degrees/s");
+            break;
+        case LSM6DS_GYRO_RANGE_2000_DPS:
+            myTrace.println("2000 degrees/s");
+            break;
+        case ISM330DHCX_GYRO_RANGE_4000_DPS:
+            myTrace.println("4000 degrees/s");
+            break;
+        }
+        // lsm6ds.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+        myTrace.print("Gyro data rate set to: ");
+        switch (lsm6ds.getGyroDataRate())
+        {
+        case LSM6DS_RATE_SHUTDOWN:
+            myTrace.println("0 Hz");
+            break;
+        case LSM6DS_RATE_12_5_HZ:
+            myTrace.println("12.5 Hz");
+            break;
+        case LSM6DS_RATE_26_HZ:
+            myTrace.println("26 Hz");
+            break;
+        case LSM6DS_RATE_52_HZ:
+            myTrace.println("52 Hz");
+            break;
+        case LSM6DS_RATE_104_HZ:
+            myTrace.println("104 Hz");
+            break;
+        case LSM6DS_RATE_208_HZ:
+            myTrace.println("208 Hz");
+            break;
+        case LSM6DS_RATE_416_HZ:
+            myTrace.println("416 Hz");
+            break;
+        case LSM6DS_RATE_833_HZ:
+            myTrace.println("833 Hz");
+            break;
+        case LSM6DS_RATE_1_66K_HZ:
+            myTrace.println("1.66 KHz");
+            break;
+        case LSM6DS_RATE_3_33K_HZ:
+            myTrace.println("3.33 KHz");
+            break;
+        case LSM6DS_RATE_6_66K_HZ:
+            myTrace.println("6.66 KHz");
+            break;
+        }
+
+        lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
+        // You can check the datarate by looking at the frequency of the DRDY pin
+        myTrace.print("Magnetometer data rate set to: ");
+        switch (lis3mdl.getDataRate())
+        {
+        case LIS3MDL_DATARATE_0_625_HZ:
+            myTrace.println("0.625 Hz");
+            break;
+        case LIS3MDL_DATARATE_1_25_HZ:
+            myTrace.println("1.25 Hz");
+            break;
+        case LIS3MDL_DATARATE_2_5_HZ:
+            myTrace.println("2.5 Hz");
+            break;
+        case LIS3MDL_DATARATE_5_HZ:
+            myTrace.println("5 Hz");
+            break;
+        case LIS3MDL_DATARATE_10_HZ:
+            myTrace.println("10 Hz");
+            break;
+        case LIS3MDL_DATARATE_20_HZ:
+            myTrace.println("20 Hz");
+            break;
+        case LIS3MDL_DATARATE_40_HZ:
+            myTrace.println("40 Hz");
+            break;
+        case LIS3MDL_DATARATE_80_HZ:
+            myTrace.println("80 Hz");
+            break;
+        case LIS3MDL_DATARATE_155_HZ:
+            myTrace.println("155 Hz");
+            break;
+        case LIS3MDL_DATARATE_300_HZ:
+            myTrace.println("300 Hz");
+            break;
+        case LIS3MDL_DATARATE_560_HZ:
+            myTrace.println("560 Hz");
+            break;
+        case LIS3MDL_DATARATE_1000_HZ:
+            myTrace.println("1000 Hz");
+            break;
+        }
+
+        lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
+        myTrace.print("Range set to: ");
+        switch (lis3mdl.getRange())
+        {
+        case LIS3MDL_RANGE_4_GAUSS:
+            myTrace.println("+-4 gauss");
+            break;
+        case LIS3MDL_RANGE_8_GAUSS:
+            myTrace.println("+-8 gauss");
+            break;
+        case LIS3MDL_RANGE_12_GAUSS:
+            myTrace.println("+-12 gauss");
+            break;
+        case LIS3MDL_RANGE_16_GAUSS:
+            myTrace.println("+-16 gauss");
+            break;
+        }
+
+        lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
+        myTrace.print("Magnetometer performance mode set to: ");
+        switch (lis3mdl.getPerformanceMode())
+        {
+        case LIS3MDL_LOWPOWERMODE:
+            myTrace.println("Low");
+            break;
+        case LIS3MDL_MEDIUMMODE:
+            myTrace.println("Medium");
+            break;
+        case LIS3MDL_HIGHMODE:
+            myTrace.println("High");
+            break;
+        case LIS3MDL_ULTRAHIGHMODE:
+            myTrace.println("Ultra-High");
+            break;
+        }
+
+        lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
+        myTrace.print("Magnetometer operation mode set to: ");
+        // Single shot mode will complete conversion and go into power down
+        switch (lis3mdl.getOperationMode())
+        {
+        case LIS3MDL_CONTINUOUSMODE:
+            myTrace.println("Continuous");
+            break;
+        case LIS3MDL_SINGLEMODE:
+            myTrace.println("Single mode");
+            break;
+        case LIS3MDL_POWERDOWNMODE:
+            myTrace.println("Power-down");
+            break;
+        }
     }
 
     void loop()
     {
         if (!isInitialized)
             return; // Skip reading if not initialized
-        // Read compass data and update heading
-        sensors_event_t event;
-        lis3mdl.getEvent(&event);
-        float heading = atan2f(event.magnetic.y, event.magnetic.x);
+        sensors_event_t accel, gyro, mag, temp;
+
+        //  /* Get new normalized sensor events */
+        lsm6ds.getEvent(&accel, &gyro, &temp);
+        lis3mdl.getEvent(&mag);
+        float heading = atan2f(mag.magnetic.y, mag.magnetic.x);
         if (heading < 0)
             heading += 2 * PI;
         float newValue = heading;
